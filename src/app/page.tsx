2524da8e5ac7f5a73409654ab823a477b6b18afc
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-01-14 20:05:09 -0700

    The seeds have been planted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,176 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.css";
 
+class Plant {
+  name: string;
+  datePlanted: Date;
+  cell?: string;
+  variant?: string;
+  dateSprouted?: Date;
+  germinationTimeframe?: {
+    start: number;
+    end: number;
+  };
+
+  constructor({
+    name,
+    datePlanted,
+    cell,
+    variant,
+  }: {
+    name: string;
+    datePlanted: Date;
+    cell: string;
+    variant?: string;
+  }) {
+    this.name = name;
+    this.datePlanted = datePlanted;
+    this.cell = cell;
+    this.variant = variant;
+  }
+}
+
 export default function Home() {
+  const oregano = new Plant({
+    name: "Oregano",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "1A",
+  });
+
+  const rosemary = new Plant({
+    name: "Rosemary",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "1B",
+  });
+
+  const basil = new Plant({
+    name: "Basil",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "1C",
+  });
+
+  const thyme = new Plant({
+    name: "Thyme",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "1D",
+  });
+
+  const pepper = new Plant({
+    name: "Bell Pepper",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "2A",
+    variant: "Orange",
+  });
+
+  const jalap = new Plant({
+    name: "Jalapeño",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "2B",
+    variant: "Black Magic",
+  });
+
+  const habanero = new Plant({
+    name: "Habanero Pepper",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "2C",
+  });
+
+  const tomato = new Plant({
+    name: "Tomato",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "2D",
+    variant: "Yellow Pear",
+  });
+
+  const chard = new Plant({
+    name: "Rainbow Chard",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "3A",
+  });
+
+  const kale = new Plant({
+    name: "Rainbow Kale",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "3B",
+  });
+
+  const lettuce = new Plant({
+    name: "Lettuce",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "3C",
+    variant: "Bibb",
+  });
+
+  const lettuceb = new Plant({
+    name: "Lettuce",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "3D",
+    variant: "Gourmet Leaf",
+  });
+
+  const arugula = new Plant({
+    name: "Arugula",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "4A",
+  });
+
+  const bean = new Plant({
+    name: "Bean",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "4B",
+    variant: "Bush",
+  });
+
+  const pac = new Plant({
+    name: "Pac Choi",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "4C",
+  });
+
+  const eggplant = new Plant({
+    name: "Eggplant",
+    datePlanted: new Date(2024, 0, 11),
+    cell: "4D",
+  });
+
+  const plants = [
+    oregano,
+    rosemary,
+    basil,
+    thyme,
+    pepper,
+    jalap,
+    habanero,
+    tomato,
+    chard,
+    kale,
+    lettuce,
+    lettuceb,
+    arugula,
+    bean,
+    pac,
+    eggplant,
+  ];
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        Welcome to The Garden
+        <div className={styles.plantGrid}>
+          {plants.map((plant) => (
+            <div
+              className={styles.plant}
+              key={`${plant.name.toString()}${plant.datePlanted.getDate()}${plant.variant?.toString()}`}
+            >
+              <p>
+                {plant.variant?.toString()} {plant.name.toString()}
+              </p>
+              <p> {plant.datePlanted.toLocaleDateString()}</p>
+            </div>
+          ))}
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
