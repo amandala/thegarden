95d9f9414af82eeedb7815890c382bef244388cb
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-01-19 14:34:04 -0700

    Add some twimkling stars to the night background

--- src/app/components/SunnyBackground.tsx
+++ src/app/components/SunnyBackground.tsx
@@ -10,6 +10,11 @@ export const SunnyBackground = () => {
         <div className={styles.crater3}></div>
       </div>
       <div className={styles.clouds}></div>
+      <div className={styles.stars}>
+        {Array.from({ length: 30 }).map((_, index) => (
+          <div key={index} className={styles.star}></div>
+        ))}
+      </div>
     </div>
   );
 };
