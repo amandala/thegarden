6565da876610cbc5ce704df943419bf27f495743
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-01-19 14:22:11 -0700

    Fix seedling leaf placement and add moon for dark mode

--- src/app/components/SunnyBackground.tsx
+++ src/app/components/SunnyBackground.tsx
@@ -4,6 +4,11 @@ export const SunnyBackground = () => {
   return (
     <div className={styles.sunnyBackground}>
       <div className={styles.sun}></div>
+      <div className={styles.moon}>
+        <div className={styles.crater1}></div>
+        <div className={styles.crater2}></div>
+        <div className={styles.crater3}></div>
+      </div>
       <div className={styles.clouds}></div>
     </div>
   );
