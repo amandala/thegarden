e428dafae54eae14aecaf9a15265596b9531e572
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-01-30 21:02:12 -0700

    Update get/plant/[id] to fetch plant from DB

--- src/app/api/garden/plant/[id]/route.ts
+++ src/app/api/garden/plant/[id]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
-import plants from "../../../data/plants.json";
 import { gardenEvents } from "@/app/events";
+import { neon } from "@neondatabase/serverless";
+import { DbPlant } from "@/app/api/types";
+import { mapPlantData } from "@/app/api/helpers/mapPlantData";
 
 export async function GET(
   _request: Request,
@@ -9,6 +11,24 @@ export async function GET(
   const id = (await params).id;
   const events = gardenEvents.filter((event) => event.plantId === id);
 
-  const plant = plants.find((plant) => plant.id === id);
+  const sql = neon(`${process.env.DATABASE_URL}`);
+
+  const dbPlants = await sql(
+    `SELECT * FROM plants as plant WHERE plant.id = '${id}'`
+  )
+    .then(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (data: Record<string, any>[]) => {
+        console.log("Plant loaded", data);
+        return data as DbPlant[];
+      }
+    )
+    .catch((error) => {
+      console.error("Error fetching plants", error);
+      return [];
+    });
+
+  const plant = mapPlantData(dbPlants)[0];
+
   return NextResponse.json({ plant, events });
 }
--- src/app/api/garden/plants/route.ts
+++ src/app/api/garden/plants/route.ts
@@ -1,26 +1,8 @@
 import { NextResponse } from "next/server";
 
 import { neon } from "@neondatabase/serverless";
-import { DbPlant, PlantData } from "../../types";
-
-const mapPlantData = (dbPlants: DbPlant[]): PlantData[] => {
-  return dbPlants.map((plant) => {
-    return {
-      id: plant.id,
-      name: plant.name,
-      datePlanted: plant.date_planted,
-      germinationTimeframe: {
-        rangeStartDays: plant.germination_range_start_days || null,
-        rangeEndDays: plant.germination_range_end_days || null,
-      },
-      location: {
-        type: plant.location_type,
-        locationId: plant.location_id,
-      },
-      variant: plant.variant,
-    };
-  });
-};
+import { DbPlant } from "../../types";
+import { mapPlantData } from "../../helpers/mapPlantData";
 
 export async function GET() {
   const sql = neon(`${process.env.DATABASE_URL}`);
