24920ee06c44c02f0ca5810546804fb7e3aadfe6
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-02-02 18:02:35 -0700

    New seed table for seed data

--- src/app/api/queries.ts
+++ src/app/api/queries.ts
@@ -1,5 +1,5 @@
-export const selectPlantsWithLocationJoin =
-  "SELECT p.id, p.name, p.date_planted, p.germination_range_start_days, p.germination_range_end_days, p.variant, l.tray_location_name, l.location_type as location_type, l.id as location_id FROM plants p JOIN location l ON p.location_id = l.id";
+export const selectPlants =
+  "SELECT p.id, s.name, p.date_planted, s.germination_range_start_days, s.germination_range_end_days, s.variant, l.tray_location_name, l.location_type, l.id as location_id FROM plants p JOIN location l ON p.location_id = l.id JOIN seeds s ON p.seed_id = s.id";
 
-export const selectPlantWithLocationJoin = (id: string) =>
-  `SELECT p.id, p.name, p.date_planted, p.germination_range_start_days, p.germination_range_end_days, p.variant, l.tray_location_name, l.location_type as location_type, l.id as location_id FROM plants p JOIN location l ON p.location_id = l.id WHERE p.id = '${id}';`;
+export const selectPlantById = (id: string) =>
+  `SELECT p.id, s.name, p.date_planted, s.germination_range_start_days, s.germination_range_end_days, s.variant, l.tray_location_name, l.location_type as location_type, l.id as location_id FROM plants p JOIN location l ON p.location_id = l.id JOIN seeds s ON p.seed_id = s.id WHERE p.id = '${id}';`;
